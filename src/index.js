44d0a5c6cf2c949973d6d2ea40804af4f4e67cc9
EphriamHab <ephremhabtamu2015@gmail.com>
2024-06-22 19:30:48 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import App from './App';
 
 import StarRating from './StarRating';
@@ -8,9 +8,9 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={['Terrible','Bad','Okay','Good','Amazing']}/>
-    <StarRating maxRating={5} color='red' size='24' className='test' defaultRating={3}/>
+    <App />
+    {/* <StarRating maxRating={5} messages={['Terrible','Bad','Okay','Good','Amazing']}/>
+    <StarRating maxRating={5} color='red' size='24' className='test' defaultRating={3}/> */}
   </React.StrictMode>
 );
 
